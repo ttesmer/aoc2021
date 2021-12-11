21aa80323171c4445129517b858adbddfb17be5f
WhateverHappns <t.tesmer@gmx.de>
2021-12-11 08:43:27 +0100

    add grid operations to utils

--- util/advent.py
+++ util/advent.py
@@ -13,22 +13,13 @@ import sys
 """
 1. Copy this file to python path:
     $ cp advent.py "`python -m site --user-site`"
-2. Import where ever you need it:
+2. Import wherever you need it:
     from advent import *
 """
 
-
-def lmap(func, *iterables):
-    return list(map(func, *iterables))
-
-
-def tr(xs: list) -> list:
-    """Transpose a list of lists:
-        >>> tr([[1,2],[3,4]])
-        [[1,3],[2,4]]
-    """
-    return lmap(list, zip(*xs))
-
+"""
+String Operations
+"""
 
 def ints(s: str) -> typing.List[int]:
     """Return ints in a string"""
@@ -43,6 +34,13 @@ def floats(s: str) -> typing.List[float]:
 def words(s: str) -> typing.List[str]:
     return re.findall(r"[a-zA-Z]+", s)
 
+"""
+List Operations
+"""
+
+def lmap(func, *iterables):
+    return list(map(func, *iterables))
+
 
 def even_indices(xs: list) -> list:
     """Return list of elements with even indices"""
@@ -56,6 +54,46 @@ def odd_indices(xs: list) -> list:
 
 def product(xs: list) -> list:
     return reduce(mul, xs)
+
+
+"""
+Grid Algorithms/Operations
+"""
+
+def neighbors(rc: tuple, m: typing.List[list],  diag=True, elem=False) -> typing.List[tuple]:
+    """Return indices or elements of neighbors in grid."""
+    r, c = rc
+    R, C = len(m), len(m[0])
+    neighbor_indices = []
+
+    if -1 < r < R and -1 < c < C:
+        if diag:
+            for dr in range(r-1, r+2):
+                for dc in range(c-1, c+2):
+                    if (0 <= dr < R) and (r != dr or c != dc) and (0 <= dc < C):
+                        neighbor_indices.append((dr, dc))
+        else:
+            for dr in range(r-1, r+2):
+                for dc in range(c-1, c+2):
+                    if (0 <= dr < R) and (r == dr and c != dc) and (0 <= dc < C):
+                        neighbor_indices.append((dr, dc))
+                    elif (0 <= dr < R) and (r != dr and c == dc) and (0 <= dc < C):
+                        neighbor_indices.append((dr, dc))
+    else:
+        raise Exception("(neighbors) index must be within grid size")
+
+    if not elem:
+        return neighbor_indices
+    else:
+        return [m[dx][dy] for dx, dy in neighbor_indices]
+
+
+def tr(xs: list) -> list:
+    """Transpose a list of lists:
+        >>> tr([[1,2],[3,4]])
+        [[1,3],[2,4]]
+    """
+    return lmap(list, zip(*xs))
 
 
 if __name__ == "__main__":
